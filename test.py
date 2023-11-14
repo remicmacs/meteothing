1f18a87164f9ec6146157a485ef6ddb0ccae5a47
remicmacs <remicmacs@protonmail.com>
2023-11-14 17:23:04 +0100

    feat(duration): compute the duration of the day
    thanks to suncalc

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ import requests
 import datetime
 import sys
 from dateutil import relativedelta as rdt
+from suncalc import get_position, get_times
+
+LON = 3.167050
+LAT = 50.683330
+TOTAL_DAY_DURATION = datetime.timedelta(days=1).total_seconds()
 
 def get_year_date_range(year):
     startdate = datetime.datetime.fromisoformat(f"{year}-01-01")
@@ -31,18 +36,37 @@ def get_min_max_temp_for_date(inputdate):
     tds_contents = [value for td in tds for value in td.contents]
     # print(tds_contents)
 
-    temps = {"min": tds_contents[1], "max": tds_contents[0]}
+    temps = {"date": inputdate,"min": tds_contents[1], "max": tds_contents[0]}
     return temps
 
+def get_day_duration(inputdate):
+    times = get_times(inputdate, LON, LAT)
+    return (times["sunset"] - times["sunrise"]).total_seconds()
+
 def main():
-    dates = get_year_date_range(2023)
-    date = next(dates)
-    temps = get_min_max_temp_for_date(date)
+    target_year = sys.argv[1]
+    dates = [
+        datetime.datetime.fromisoformat("2022-01-01"),
+        datetime.datetime.fromisoformat("2022-01-02"),
+        # datetime.datetime.fromisoformat("2022-01-03"),
+        # datetime.datetime.fromisoformat("2022-01-04"),
+        # datetime.datetime.fromisoformat("2022-01-05"),
+        # datetime.datetime.fromisoformat("2022-01-06"),
+        # datetime.datetime.fromisoformat("2022-01-07"),
+        # datetime.datetime.fromisoformat("2022-01-08"),
+    ]
+    # dates = get_year_date_range(int(target_year))
+    temps = [ {**get_min_max_temp_for_date(date), "day_duration": get_day_duration(date)} for date in dates]
+
     print(temps)
 
-    print("I would also have fetched min max temps for dates:")
-    for date in dates:
-        print(date.strftime("%Y-%m-%d"))
+    # date = next(dates)
+    # temps = get_min_max_temp_for_date(date)
+    # print(temps)
+    #
+    # print("I would also have fetched min max temps for dates:")
+    # for date in dates:
+    #     print(date.strftime("%Y-%m-%d"))
 
 if __name__=="__main__": 
     main()
