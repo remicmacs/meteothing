a8689daa2e5a0eddaa51ca736b7b5c189610764c
remicmacs <remicmacs@protonmail.com>
2023-11-14 16:30:54 +0100

    feat(dataviz): colomn with two parts

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ from matplotlib.colors import ListedColormap
 import pandas as pd
 
 FIRST = 1850
-LAST = 2018  # inclusive
+LAST = 2023  # inclusive
 
 
 # Reference period for the center of the color scale
@@ -19,17 +19,31 @@ LIM = 0.7 # degrees
 # data from
 
 # https://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/time_series/HadCRUT.4.6.0.0.annual_ns_avg.txt
+#
+# df = pd.read_fwf(
+#     'HadCRUT.4.6.0.0.annual_ns_avg.txt',
+#     index_col=0,
+#     usecols=(0, 1),
+#     names=['year', 'anomaly'],
+#     header=None,
+# )
 
-df = pd.read_fwf(
-    'HadCRUT.4.6.0.0.annual_ns_avg.txt',
+df = pd.read_csv(
+    "HadCRUT.5.0.1.0.analysis.summary_series.global.annual.csv",
+    # Year column is the index column
     index_col=0,
-    usecols=(0, 1),
-    names=['year', 'anomaly'],
-    header=None,
+    # Use only year and anomaly column
+    usecols=(0, 1, 2),
+    names=['year', 'anomaly', 'other'],
+    # There is a header in this CSV file, skip it and use names arg
+    header=0
 )
 
 anomaly = df.loc[FIRST:LAST, 'anomaly'].dropna()
 reference = anomaly.loc[FIRST_REFERENCE:LAST_REFERENCE].mean()
+
+other = df.loc[FIRST:LAST, 'other'].dropna()
+other_reference = other.loc[FIRST_REFERENCE:LAST_REFERENCE].mean()
 
 # the colors in this colormap come from http://colorbrewer2.org
 
@@ -42,7 +56,7 @@ cmap = ListedColormap([
     '#ef3b2c', '#cb181d', '#a50f15', '#67000d',
 ])
 
-fig = plt.figure(figsize=(10, 1))
+fig = plt.figure(figsize=(10, 2))
 
 ax = fig.add_axes([0, 0, 1, 1])
 ax.set_axis_off()
@@ -61,7 +75,16 @@ col.set_cmap(cmap)
 col.set_clim(reference - LIM, reference + LIM)
 ax.add_collection(col)
 
-ax.set_ylim(0, 1)
+upper_col = PatchCollection([
+    Rectangle((y, 1), 1, 1)
+    for y in range(FIRST, LAST + 1)
+])
+upper_col.set_array(other)
+upper_col.set_cmap(cmap)
+upper_col.set_clim(other_reference - LIM, other_reference + LIM)
+ax.add_collection(upper_col)
+
+ax.set_ylim(0, 2)
 ax.set_xlim(FIRST, LAST + 1)
 
 fig.savefig('warming-stripes.png')
